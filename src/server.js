5fbd971f862629469e43568af1bd7ea54aa32b5f
kwonsigyeong <87909447+kwonsigyeong@users.noreply.github.com>
2022-03-12 00:46:04 +0900

    express

--- src/server.js
+++ src/server.js
@@ -1,16 +1,14 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const handleHome = (req, res) => {
-  return res.send("adfadsfasd");
-};
-
-app.get("/", handleHome);
+app.use(logger);
 
 const handleListening = () =>
-  console.log(`✔Server listening on port http://localhost:${PORT} 🐱‍🏍`);
+  console.log(`✔Server listening on port http://localhost:${PORT} 🎈`);
 
 app.listen(PORT, handleListening);
